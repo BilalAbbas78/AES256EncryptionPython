db03eb8649fa76face37ea545372e0347c28cc6f
Syed Bilal Abbas <abbasb000@gmail.com>
2022-11-02 00:36:11 +0500

    Padding issue removed

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from base64 import b64decode
 from base64 import b64encode
 from Crypto.Cipher import AES
 import os
+import time
+from Crypto.Util.Padding import pad, unpad
 
-# Padding for the input string --not
-# related to encryption itself.
 BLOCK_SIZE = 16  # Bytes
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
-                chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 # Random 256-bit key
 random_key = os.urandom(32)
@@ -27,35 +24,21 @@ class AESCipher:
         self.key = md5(key).hexdigest()
 
     def encrypt(self, raw):
-        raw = pad(raw)
+        raw = pad(raw, BLOCK_SIZE)
         cipher = AES.new(self.key.encode("utf8"), AES.MODE_ECB)
-        return b64encode(cipher.encrypt(raw.encode('utf8')))
+        return b64encode(cipher.encrypt(raw))
 
     def decrypt(self, enc):
         enc = b64decode(enc)
         cipher = AES.new(self.key.encode("utf8"), AES.MODE_ECB)
-        return unpad(cipher.decrypt(enc)).decode('utf8')
+        return unpad(cipher.decrypt(enc), BLOCK_SIZE).decode('utf8')
 
 
 # MAIN
-# msg = input('Message...: ')
-# # pwd = input('Password..: ')
-#
-# cipherText = AESCipher(random_key).encrypt(msg)
-# print(random_key)
-#
-# print(cipherText)
-#
-# decipher = AESCipher(random_key).decrypt(cipherText)
-#
-# print(decipher)
-# # print('Ciphertext:', AESCipher(pwd).encrypt(msg))
-# # print('Ciphertext:', AESCipher(pwd).decrypt(msg))
+start = time.time()
 
-fileRead = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\test.txt", "r")
-fileWrite = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\myfile.txt", "w+")
-
-# print(f.readline())
+fileRead = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\test2.txt", "r", encoding="utf8")
+fileWrite = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\myfile.txt", "w+", encoding="utf8")
 
 # reading each line
 for line in fileRead:
@@ -64,17 +47,20 @@ for line in fileRead:
     for word in line.split():
         # displaying the words
         # print(word)
-        fileWrite.write(AESCipher(random_key).encrypt(word).decode("utf-8") + " ")
+        fileWrite.write(AESCipher(random_key).encrypt(word.encode("utf8")).decode("utf8") + " ")
     fileWrite.write("\n")
 
 fileWrite.close()
 
-print("Reading encrypted file")
-fileRead = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\myfile.txt", "r")
+end = time.time()
+print("Encryption time: %.2f seconds" % round(end - start, 2))
+
+print("Reading encrypted file:\n")
+fileRead = open("C:\\Users\\Syed Bilal Abbas\\Desktop\\myfile.txt", "r", encoding="utf8")
 for line in fileRead:
     for word in line.split():
         # print(word)
-        print(AESCipher(random_key).decrypt(word.encode("utf-8")) + " ", end='')
+        print(AESCipher(random_key).decrypt(word.encode("utf8")) + " ", end='')
     print()
 
 fileWrite.close()
